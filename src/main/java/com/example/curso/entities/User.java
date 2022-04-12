575980455e8f8aff814642e41c88e9e9fe15a205
NicolasPrates <nicolaspratesaraujo@gmail.com>
2022-04-12 14:46:46 -0300

    implementando o banco de testes h2

--- src/main/java/com/example/curso/entities/User.java
+++ src/main/java/com/example/curso/entities/User.java
@@ -1,11 +1,19 @@
 package com.example.curso.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.io.Serial;
 import java.io.Serializable;
 
+@Entity
 public class User implements Serializable {
     @Serial
     private static final long serialVersionUID = 1l;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
