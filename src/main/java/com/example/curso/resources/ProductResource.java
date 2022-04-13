9e510b6ed2d1c202210afb0b98066828099b90ce
NicolasPrates <nicolaspratesaraujo@gmail.com>
2022-04-13 17:01:44 -0300

    classe produtos

---
+++ src/main/java/com/example/curso/resources/ProductResource.java
@@ -0,0 +1,34 @@
+package com.example.curso.resources;
+
+import com.example.curso.entities.Product;
+import com.example.curso.services.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/products")
+public class ProductResource {
+
+    private final ProductService service;
+
+    @Autowired
+    public ProductResource(ProductService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Product>> findAll() {
+        List<Product> list = service.findAll();
+
+        return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Product> findById(@PathVariable Long id){
+        Product obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
+    }
+}
